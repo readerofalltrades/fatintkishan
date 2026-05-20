9f0ec1717890f4d8031138b6343e002add421ea4
readeroaltrades <fatintkishan@gmail.com>
2026-05-20 23:55:27 +0600

    phase 1.5: hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
 import './Navbar.css'
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <a href="/" className="navbar-logo"><span className='cursor'>_</span>readerofalltrades</a>
-      <ul className="navbar-links">
-        <li><a href="/projects">Work</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/blog">Blog</a></li>
+
+      <button
+        className={`navbar-hamburger ${menuOpen ? 'navbar-hamburger--open' : ''}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle menu"
+        aria-expanded={menuOpen}
+      >
+        <span /><span /><span />
+      </button>
+
+      <ul className={`navbar-links ${menuOpen ? 'navbar-links--open' : ''}`}>
+        <li><a href="/projects" onClick={() => setMenuOpen(false)}>Work</a></li>
+        <li><a href="/about" onClick={() => setMenuOpen(false)}>About</a></li>
+        <li><a href="/blog" onClick={() => setMenuOpen(false)}>Blog</a></li>
       </ul>
     </nav>
   )
